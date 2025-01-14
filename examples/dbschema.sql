f1f0f1a98c9442511db4289215d17c3298643f14
creakz <kaktotak587@gmail.com>
2025-01-14 22:18:41 +0300

    added database schema example

---
+++ examples/dbschema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS members (
+  id SERIAL PRIMARY KEY,
+  nickname VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(60) NOT NULL,
+  join_date TIMESTAMP NOT NULL,
+  sex VARCHAR(30),
+  about TEXT
+);
+
+CREATE TABLE IF NOT EXISTS positions (
+  id SERIAL PRIMARY KEY,
+  position TEXT
+);
+
+CREATE TABLE IF NOT EXISTS admins (
+  id SERIAL PRIMARY KEY,
+  nickname VARCHAR(50) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  position_id INTEGER NOT NULL REFERENCES positions(id),
+  join_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS invite_tokens (
+  id SERIAL PRIMARY KEY,
+  token VARCHAR(30)
+);
